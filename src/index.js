58990e9e4c56282f1b26e23e77aae9eb675d177e
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2021-12-22 12:04:37 -0800

    update formatting on code

--- src/index.js
+++ src/index.js
@@ -1,245 +1,257 @@
 import "./style.css";
-import {Task, TaskFolder, DefaultData} from "./objects";
+import { Task, TaskFolder, DefaultData } from "./objects";
 
 let collection = [];
 
 // Print folder names to sidebar section
 function printSidebar() {
-    const sidebar = document.getElementById("sidebar");
-    sidebar.innerHTML = '';
+	const sidebar = document.getElementById("sidebar");
+	sidebar.innerHTML = "";
 
-    collection.map((folder) => {
-        const p = document.createElement("p");
-        p.textContent = folder.getFolderName();
-        p.addEventListener("click", () => {
-            displayTasks(folder.getFolderName());
-        });
+	collection.map((folder) => {
+		const p = document.createElement("p");
+		p.textContent = folder.getFolderName();
+		p.addEventListener("click", () => {
+			displayTasks(folder.getFolderName());
+		});
 
-        sidebar.appendChild(p);
-    })
+		sidebar.appendChild(p);
+	});
 }
 
 // Display a specific folders tasks to the main container. Calls "default" folder on boot up
 // and then get's recalled anytime user selects a different folder to view.
 function displayTasks(folderName) {
-    // This clears any currently displayed tasks that was selected on the right sidebar.
-    const rightSidebar = document.getElementById("rightSidebar");
-    rightSidebar.innerHTML = "";
+	// This clears any currently displayed tasks that was selected on the right sidebar.
+	const rightSidebar = document.getElementById("rightSidebar");
+	rightSidebar.innerHTML = "";
 
-    collection.map( (folder) => {
-        // If we find the folder requested we print out its task
-        if (folder.getFolderName() === folderName.toUpperCase()) {
-            // Clear current task displayed
-            const taskContainer = document.getElementById("tasksContainer");
-            taskContainer.innerHTML = "";
+	collection.map((folder) => {
+		// If we find the folder requested we print out its task
+		if (folder.getFolderName() === folderName.toUpperCase()) {
+			// Clear current task displayed
+			const taskContainer = document.getElementById("tasksContainer");
+			taskContainer.innerHTML = "";
 
-            // Start grabbing new task to be displayed
-            const currentTasks = folder.getTasks();
-            currentTasks.map( (task) => {
-                // Create the task "card" to display all of its contents
-                const taskCard = document.createElement("div");
-                taskCard.classList.add("taskCard");
-                // For card styling reference and to be used by addPriorityStyling function.
-                let priorityAttribute = "";
+			// Start grabbing new task to be displayed
+			const currentTasks = folder.getTasks();
+			currentTasks.map((task) => {
+				// Create the task "card" to display all of its contents
+				const taskCard = document.createElement("div");
+				taskCard.classList.add("taskCard");
+				// For card styling reference and to be used by addPriorityStyling function.
+				let priorityAttribute = "";
 
-                const contents = task.returnInArray();
-                contents.map((key, index)=>{
-                    const p = document.createElement("p");
+				const contents = task.returnInArray();
+				contents.map((key, index) => {
+					const p = document.createElement("p");
 
-                    // Means we are currently adding the "priority" section to the taskCard.
-                    if(index === 1) {
-                        p.classList.add("priorityKey");
-                        priorityAttribute = key;
-                    }
+					// Means we are currently adding the "priority" section to the taskCard.
+					if (index === 1) {
+						p.classList.add("priorityKey");
+						priorityAttribute = key;
+					}
 
-                    p.textContent = key;
-                    taskCard.appendChild(p);
-                });
+					p.textContent = key;
+					taskCard.appendChild(p);
+				});
 
-                // Add event listener, so when clicked, it's full details will be displayed in right sidebar.
-                taskCard.addEventListener("click", ()=> {
-                    displayDetails(folder.getFolderName(), task.getTitle());
-                });
+				// Add event listener, so when clicked, it's full details will be displayed in right sidebar.
+				taskCard.addEventListener("click", () => {
+					displayDetails(folder.getFolderName(), task.getTitle());
+				});
 
-                // Add button that allows this item to be deleted.
-                taskCard.appendChild(addDeleteButton(folder.getFolderName(), task.getTitle()));
+				// Add button that allows this item to be deleted.
+				taskCard.appendChild(
+					addDeleteButton(folder.getFolderName(), task.getTitle())
+				);
 
-                addPriorityStyling(taskCard, priorityAttribute);
-                taskContainer.appendChild(taskCard);
-            })
-        }
-    })
+				addPriorityStyling(taskCard, priorityAttribute);
+				taskContainer.appendChild(taskCard);
+			});
+		}
+	});
 }
 
 // Will style the task card background depending on priority.
 function addPriorityStyling(taskCard, priorityAttribute) {
-    if (priorityAttribute === "important") {
-        taskCard.classList.add("importantPriority");
-    }
-
+	if (priorityAttribute === "important") {
+		taskCard.classList.add("importantPriority");
+	}
 }
 
 // Returns a button and when clicked on, deletes the specific task from whichever folder it belongs to.
 function addDeleteButton(folderName, taskTitle) {
-    const btn = document.createElement("button");
-    btn.classList.add("deleteTask");
-    btn.textContent = "Delete";
+	const btn = document.createElement("button");
+	btn.classList.add("deleteTask");
+	btn.textContent = "Delete";
 
-    btn.addEventListener("click", (e)=> {
-        let indexToDelete = -1;
-        collection.map( (folder)=> {
-            if (folder.getFolderName() === folderName) {
-                folder.getTasks().map( (task, taskIndex) => {
-                    if (task.getTitle() === taskTitle) {
-                        indexToDelete = taskIndex;
-                    }
-                })
-            }
-        })
-        deleteTask(folderName, indexToDelete);
-        displayTasks(folderName);
-    })
-    return btn;
+	btn.addEventListener("click", (e) => {
+		let indexToDelete = -1;
+		collection.map((folder) => {
+			if (folder.getFolderName() === folderName) {
+				folder.getTasks().map((task, taskIndex) => {
+					if (task.getTitle() === taskTitle) {
+						indexToDelete = taskIndex;
+					}
+				});
+			}
+		});
+		deleteTask(folderName, indexToDelete);
+		displayTasks(folderName);
+	});
+	return btn;
 }
 
 // Helper function for task deletion method
 function deleteTask(folderName, taskIndex) {
-    collection.map( (folder) => {
-        if (folder.getFolderName() === folderName) {
-            folder.deleteTaskByIndex(taskIndex);
-        }
-    })
+	collection.map((folder) => {
+		if (folder.getFolderName() === folderName) {
+			folder.deleteTaskByIndex(taskIndex);
+		}
+	});
 }
 
 // Anytime a specific task is clicked on, this function displays all of its contents on the right sidebar.
 function displayDetails(folderName, taskName) {
-    const rightSidebar = document.getElementById("rightSidebar");
-    rightSidebar.innerHTML = "";
+	const rightSidebar = document.getElementById("rightSidebar");
+	rightSidebar.innerHTML = "";
 
-    collection.map( (folder) => {
-        if (folder.getFolderName() === folderName) {
-            const folderTasks = folder.getTasks();
-            folderTasks.map( (task) => {
-                if(task.getTitle() === taskName) {
-                    // Gotcha
-                    task.returnAll().map( (key, index) => {
-                        const desc = document.createElement("p");
-                        desc.classList.add("detailsKeys")
-                        desc.textContent = Object.keys(task)[index].toUpperCase() + ":";
+	collection.map((folder) => {
+		if (folder.getFolderName() === folderName) {
+			const folderTasks = folder.getTasks();
+			folderTasks.map((task) => {
+				if (task.getTitle() === taskName) {
+					// Gotcha
+					task.returnAll().map((key, index) => {
+						const desc = document.createElement("p");
+						desc.classList.add("detailsKeys");
+						desc.textContent = Object.keys(task)[index].toUpperCase() + ":";
 
-                        const  p = document.createElement("p");
-                        p.classList.add("taskDetails");
-                        p.textContent = key;
+						const p = document.createElement("p");
+						p.classList.add("taskDetails");
+						p.textContent = key;
 
-                        rightSidebar.appendChild(desc);
-                        rightSidebar.appendChild(p);
-                        // Add button that allows user to modify a task.
-                        rightSidebar.appendChild(addModifyTaskButton(folderName, taskName, task, Object.keys(task)[index]));
+						rightSidebar.appendChild(desc);
+						rightSidebar.appendChild(p);
+						// Add button that allows user to modify a task.
+						rightSidebar.appendChild(
+							addModifyTaskButton(
+								folderName,
+								taskName,
+								task,
+								Object.keys(task)[index]
+							)
+						);
 
-                        rightSidebar.appendChild(document.createElement("br"));
-                    })
-                }
-            })
-        }
-    })
+						rightSidebar.appendChild(document.createElement("br"));
+					});
+				}
+			});
+		}
+	});
 }
 
 // Will add a modify task button that shows a form in order to update details.
 function addModifyTaskButton(folderName, taskName, task, keyToUpdate) {
-    const btn = document.createElement("button");
-    btn.classList.add("modifyTaskButton");
-    btn.textContent = "Edit";
-    btn.addEventListener("click", () =>{
-        const newVal = prompt("Enter New Value:");
-        if (keyToUpdate === "title") {
-            task.setTitle(newVal);
-        } else if (keyToUpdate === "description") {
-            task.setDescription(newVal);
-        } else if (keyToUpdate === "priority") {
-            task.setPriority(newVal);
-        }
-    
-        // Re-display updated task value(s)
-        displayTasks(folderName);
-        displayDetails(folderName, taskName);
-    })
+	const btn = document.createElement("button");
+	btn.classList.add("modifyTaskButton");
+	btn.textContent = "Edit";
+	btn.addEventListener("click", () => {
+		const newVal = prompt("Enter New Value:");
+		if (keyToUpdate === "title") {
+			task.setTitle(newVal);
+		} else if (keyToUpdate === "description") {
+			task.setDescription(newVal);
+		} else if (keyToUpdate === "priority") {
+			task.setPriority(newVal);
+		}
 
-    return btn;
+		// Re-display updated task value(s)
+		displayTasks(folderName);
+		displayDetails(folderName, taskName);
+	});
+
+	return btn;
 }
 
 // Toggle display of form to submit a new one
 const popupForm = document.getElementById("popupForm");
-popupForm.addEventListener("click", ()=> {
-    togglePopup();
+popupForm.addEventListener("click", () => {
+	togglePopup();
 });
 function togglePopup() {
-    const form = document.getElementById("submitNewTaskForm");
-    
-    // Before we display the tasks - we must add all available folders - also clears any previous options.
-    const select = document.getElementById("folder");
-    select.innerHTML = "";
+	const form = document.getElementById("submitNewTaskForm");
 
-    collection.map((folder) => {
-        const option = document.createElement("option");
-        option.value = folder.getFolderName();
-        option.textContent = folder.getFolderName();
+	// Before we display the tasks - we must add all available folders - also clears any previous options.
+	const select = document.getElementById("folder");
+	select.innerHTML = "";
 
-        select.appendChild(option);
-    });
+	collection.map((folder) => {
+		const option = document.createElement("option");
+		option.value = folder.getFolderName();
+		option.textContent = folder.getFolderName();
 
-    displayToggle(form);
+		select.appendChild(option);
+	});
+
+	displayToggle(form);
 }
 // Add new task to our array after completion of form
 const btn = document.getElementById("newTask");
 btn.addEventListener("click", (e) => {
-    e.preventDefault(); 
-    // Create New task
-    const newTask = new Task(e.target.form.title.value, e.target.form.description.value, e.target.form.priority.value);
-    
-    // Find folder that user is adding task to
-    collection.map( (folder) => {
-        if (folder.getFolderName() === e.target.form.folder.value) {
-            folder.addTask(newTask);
-        }
-    })
+	e.preventDefault();
+	// Create New task
+	const newTask = new Task(
+		e.target.form.title.value,
+		e.target.form.description.value,
+		e.target.form.priority.value
+	);
 
-    // Display the contents of notes of selected folder
-    displayTasks(e.target.form.folder.value);
+	// Find folder that user is adding task to
+	collection.map((folder) => {
+		if (folder.getFolderName() === e.target.form.folder.value) {
+			folder.addTask(newTask);
+		}
+	});
 
-    // Hides the form
-    togglePopup();
+	// Display the contents of notes of selected folder
+	displayTasks(e.target.form.folder.value);
+
+	// Hides the form
+	togglePopup();
 });
 
 // Add new folder to our collection
 const newFolderBtn = document.getElementById("addFolder");
 newFolderBtn.addEventListener("click", (e) => {
-    const form = document.getElementById("submitNewFolder");
-    displayToggle(form);
+	const form = document.getElementById("submitNewFolder");
+	displayToggle(form);
 });
 const submitNewFolderBtn = document.getElementById("newFolder");
-submitNewFolderBtn.addEventListener("click", (e)=> {
-    e.preventDefault();
-    const newFolder = new TaskFolder(e.target.form.name.value);
-    collection.push(newFolder);
+submitNewFolderBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+	const newFolder = new TaskFolder(e.target.form.name.value);
+	collection.push(newFolder);
 
-    printSidebar();    
+	printSidebar();
 
-    const form = document.getElementById("submitNewFolder")
-    displayToggle(form);
+	const form = document.getElementById("submitNewFolder");
+	displayToggle(form);
 });
 
 function displayToggle(form) {
-    if (form.style.display === "none" || form.style.display === "") {
-        form.style.display = "block";
-    } else {
-        form.style.display = "none";
-    }
+	if (form.style.display === "none" || form.style.display === "") {
+		form.style.display = "block";
+	} else {
+		form.style.display = "none";
+	}
 }
 
 // Default Startup
 const loadDefaultData = DefaultData.returnData();
-loadDefaultData.map( (dataPoint) => {
-    collection.push(dataPoint);
+loadDefaultData.map((dataPoint) => {
+	collection.push(dataPoint);
 });
 printSidebar();
 displayTasks("tasks");
--- src/objects.js
+++ src/objects.js
@@ -1,88 +1,107 @@
 class Task {
-    constructor(titleIn, descriptionIn, priorityIn) {
-        this.title = titleIn;
-        this.description = descriptionIn;
-        this.priority = priorityIn;
-    }
+	constructor(titleIn, descriptionIn, priorityIn) {
+		this.title = titleIn;
+		this.description = descriptionIn;
+		this.priority = priorityIn;
+	}
 
-    getTitle() {
-        return this.title;
-    }
+	getTitle() {
+		return this.title;
+	}
 
-    setTitle(newTitle) {
-        this.title = newTitle;
-    }
+	setTitle(newTitle) {
+		this.title = newTitle;
+	}
 
-    setDescription(newDesc) {
-        this.description = newDesc;
-    }
+	setDescription(newDesc) {
+		this.description = newDesc;
+	}
 
-    setPriority(newPri) {
-        this.priority = newPri;
-    }
+	setPriority(newPri) {
+		this.priority = newPri;
+	}
 
-    returnInArray() {
-        return [this.title, this.priority];
-    }
+	returnInArray() {
+		return [this.title, this.priority];
+	}
 
-    returnAll() {
-        return [this.title, this.description, this.priority];
-    }
+	returnAll() {
+		return [this.title, this.description, this.priority];
+	}
 }
 
 class TaskFolder {
-    constructor(folderName) {
-        this.folderName = folderName.toUpperCase();
-        this.myTasks = [];
-    }
+	constructor(folderName) {
+		this.folderName = folderName.toUpperCase();
+		this.myTasks = [];
+	}
 
-    addTask(Task) {
-        this.myTasks.push(Task);
-    }
+	addTask(Task) {
+		this.myTasks.push(Task);
+	}
 
-    getTasks() {
-        return this.myTasks;
-    }
+	getTasks() {
+		return this.myTasks;
+	}
 
-    deleteTaskByIndex(taskIndex) {
-        this.myTasks.splice(taskIndex, 1);
-    }
+	deleteTaskByIndex(taskIndex) {
+		this.myTasks.splice(taskIndex, 1);
+	}
 
-    getFolderName() {
-        return this.folderName;
-    }
+	getFolderName() {
+		return this.folderName;
+	}
 }
 
 const DefaultData = (() => {
-    const newTask = new Task("Stay Healthy", "run 2 miles after school", "important");
-    const newTaskTwo = new Task("Submit Final Paper", "ask jon to revise my essay and submit paper to prof", "important");
-    const newTaskThree = new Task("Order Extra Coffee Pods", "buy bulk from sams for extra savings", "somewhat important");
-    const newTaskFour = new Task("Monthly Financial Report", "submit excel report of this months finances to bob", "important");
-    const newTaskFive = new Task("Replacement Hat", "lost my had need new one", "not important");
+	const newTask = new Task(
+		"Stay Healthy",
+		"run 2 miles after school",
+		"important"
+	);
+	const newTaskTwo = new Task(
+		"Submit Final Paper",
+		"ask jon to revise my essay and submit paper to prof",
+		"important"
+	);
+	const newTaskThree = new Task(
+		"Order Extra Coffee Pods",
+		"buy bulk from sams for extra savings",
+		"somewhat important"
+	);
+	const newTaskFour = new Task(
+		"Monthly Financial Report",
+		"submit excel report of this months finances to bob",
+		"important"
+	);
+	const newTaskFive = new Task(
+		"Replacement Hat",
+		"lost my had need new one",
+		"not important"
+	);
 
-    const defaultTasks = new TaskFolder("tasks");
-    const thisWeek = new TaskFolder("this week");  
-    const thisMonth = new TaskFolder("this month"); 
+	const defaultTasks = new TaskFolder("tasks");
+	const thisWeek = new TaskFolder("this week");
+	const thisMonth = new TaskFolder("this month");
 
-    const setupDefault = () => {
-        defaultTasks.addTask(newTask);
-        defaultTasks.addTask(newTaskTwo);
-        defaultTasks.addTask(newTaskThree);
-        defaultTasks.addTask(newTaskFive);
+	const setupDefault = () => {
+		defaultTasks.addTask(newTask);
+		defaultTasks.addTask(newTaskTwo);
+		defaultTasks.addTask(newTaskThree);
+		defaultTasks.addTask(newTaskFive);
 
+		thisWeek.addTask(newTaskTwo);
+		thisWeek.addTask(newTaskFour);
+	};
 
-        thisWeek.addTask(newTaskTwo);
-        thisWeek.addTask(newTaskFour);
-    }
+	const returnData = () => {
+		setupDefault();
+		return [defaultTasks, thisWeek, thisMonth];
+	};
 
-    const returnData = () => {
-        setupDefault();
-        return [defaultTasks, thisWeek, thisMonth];
-    }
-
-    return {
-        returnData,
-    }
+	return {
+		returnData,
+	};
 })();
 
-export {Task, TaskFolder, DefaultData}
+export { Task, TaskFolder, DefaultData };
